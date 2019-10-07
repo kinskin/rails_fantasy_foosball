0bd9d3bb131ff0c6b78ce939e2e77bf02758d70b
kinskin <md.asshikin@gmail.com>
2019-10-07 17:36:37 +0800

    resolved routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "teams#index"
 
-  resources :homes
   resources :teams
-  resources :stages
-  resources :scores
-  resources :eliminations
-  resources :quarters
-  resources :semis
-  resources :finals
   resources :scoreboards
 end
